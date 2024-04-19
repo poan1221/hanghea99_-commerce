1925628b0398d10dae4c56e3996f9feab6ef56ad
haSong <70552536+poan1221@users.noreply.github.com>
2024-04-20 00:10:26 +0900

    firebase init

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: `${process.env.VITE_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.VITE_APP_FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${process.env.VITE_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.VITE_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.VITE_APP_FIREBASE_APP_ID}`,
+  measurementId: `${process.env.VITE_APP_FIREBASE_MEASUREMENT_ID}`,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
