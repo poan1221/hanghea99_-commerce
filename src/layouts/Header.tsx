4beef808cb74889b1b5e48519e307bfc19b270c2
haSong <70552536+poan1221@users.noreply.github.com>
2024-04-20 15:48:34 +0900

    [feature] 로그인페이지 디자인, 적용

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -5,29 +5,31 @@ export const Header = () => {
   const { moveHome, moveCart, moveLogin, moveSignup } = useNavigate();
 
   return (
-    <div className="nav flex justify-between">
-      <div className="text-left">
-        <h1 className="nav-title hover" onClick={() => moveHome()}>
-          plaveshop
-        </h1>
-        <Navigation />
-      </div>
-      <div className="flex-center">
-        <div className="flex gap-15">
-          <button className="nav-button hover" onClick={() => moveSignup()}>
-            회원가입
-          </button>
-          <button className="nav-button hover" onClick={() => moveLogin()}>
-            로그인
-          </button>
+    <header className="border-b border-slate-200">
+      <div className="max-w-1200 nav flex justify-between">
+        <div className="text-left">
+          <h1 className="nav-title hover" onClick={() => moveHome()}>
+            plaveshop
+          </h1>
+          <Navigation />
         </div>
-        <div className="flex gap-15">
-          <button className="nav-button hover">위시리스트</button>
-          <button className="nav-button hover" onClick={() => moveCart()}>
-            장바구니
-          </button>
+        <div className="flex-center">
+          <div className="flex gap-15">
+            <button className="nav-button hover" onClick={() => moveSignup()}>
+              회원가입
+            </button>
+            <button className="nav-button hover" onClick={() => moveLogin()}>
+              로그인
+            </button>
+          </div>
+          <div className="flex gap-15">
+            <button className="nav-button hover">위시리스트</button>
+            <button className="nav-button hover" onClick={() => moveCart()}>
+              장바구니
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </header>
   );
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,72 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "../hook/useNavigate";
+
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+interface IForm {
+  email: string;
+  password: string;
+}
+
 export const Login = () => {
+  const { moveSignup } = useNavigate();
+
+  const form = useForm<IForm>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const email = form.watch("email");
+  const password = form.watch("password");
+  const allWrite = !email || !password;
+
+  function onSubmit(values: IForm) {
+    // 로그인 기능 추가 _ firebase 사용
+  }
+
   return (
-    <section>
-      <div>Login</div>
+    <section className="max-w-lg section-py mx-auto">
+      <h2 className="text-slate-900 font-bold text-[40px] pb-10">LOGIN</h2>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="email" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input type="password" placeholder="password" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full" disabled={allWrite}>
+            로그인
+          </Button>
+          <div
+            className="text-right cursor-pointer"
+            onClick={() => moveSignup()}
+          >
+            아직 회원이 아니신가요?
+          </div>
+        </form>
+      </Form>
     </section>
   );
 };
