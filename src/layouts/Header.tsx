24afd8b5f47ff7da0c7225f06ee4c4a11e7ff36f
haSong <70552536+poan1221@users.noreply.github.com>
2024-04-23 15:50:57 +0900

    feat. 헤더 로그아웃 추가

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,8 +1,17 @@
+import { useUserStore } from "@/store/useUserStore";
 import { useNavigate } from "../hook/useNavigate";
 import { Navigation } from "../router/Navigation";
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
 
 export const Header = () => {
   const { moveHome, moveCart, moveLogin, moveSignup } = useNavigate();
+  const user = useUserStore((state) => state.user);
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    moveHome();
+  };
 
   return (
     <header className="border-b border-slate-200">
@@ -14,20 +23,36 @@ export const Header = () => {
           <Navigation />
         </div>
         <div className="flex-center">
-          <div className="flex gap-15">
-            <button className="nav-button hover" onClick={() => moveSignup()}>
-              회원가입
-            </button>
-            <button className="nav-button hover" onClick={() => moveLogin()}>
-              로그인
-            </button>
-          </div>
-          <div className="flex gap-15">
-            <button className="nav-button hover">위시리스트</button>
-            <button className="nav-button hover" onClick={() => moveCart()}>
-              장바구니
-            </button>
-          </div>
+          {user ? (
+            <div className="flex gap-15">
+              <button className="nav-button hover" onClick={handleLogout}>
+                로그아웃
+              </button>
+            </div>
+          ) : (
+            <>
+              <div className="flex gap-15">
+                <button
+                  className="nav-button hover"
+                  onClick={() => moveSignup()}
+                >
+                  회원가입
+                </button>
+                <button
+                  className="nav-button hover"
+                  onClick={() => moveLogin()}
+                >
+                  로그인
+                </button>
+              </div>
+              <div className="flex gap-15">
+                <button className="nav-button hover">위시리스트</button>
+                <button className="nav-button hover" onClick={() => moveCart()}>
+                  장바구니
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </header>
