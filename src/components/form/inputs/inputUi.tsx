7e54dae06fbbf79f71687e7335d3417d3c4f8c59
haSong <70552536+poan1221@users.noreply.github.com>
2024-05-08 00:17:26 +0900

    feat. 리스트 페이지 타이틀 정렬 추가, 상품 미존재 시 영역 추가

---
+++ src/components/form/inputs/inputUi.tsx
@@ -0,0 +1,6 @@
+export interface productFormInputProps {
+  id: string;
+  label: string;
+  type: string;
+  placeholder?: string;
+}
--- src/pages/product/SelectedList.tsx
+++ src/pages/product/SelectedList.tsx
@@ -2,6 +2,7 @@ import { useGetSelectedProducts } from "@/api/productQueries";
 import { ProductCard } from "@/components/Product/ProductCard";
 import ProductCardSkelton from "@/components/Product/ProductCardSkelecton";
 import { PageTitle } from "@/components/common/PageTItle";
+import { addSpaceSeriesTitle } from "@/utils/addSpaceSeriesTitle";
 import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import { useParams } from "react-router-dom";
@@ -9,6 +10,14 @@ import { useParams } from "react-router-dom";
 export const ProductSelectedList = () => {
   const selectedType = useParams().id;
   const { ref, inView } = useInView();
+
+  const getTitle = (selectedType: string) => {
+    if (selectedType === "OfficeSupplies") {
+      return "Office Items";
+    } else {
+      return addSpaceSeriesTitle(selectedType);
+    }
+  };
 
   const {
     data,
@@ -27,10 +36,15 @@ export const ProductSelectedList = () => {
 
   return (
     <section className="container productsWrap max-w-4xl mt-11 mx-auto">
-      <PageTitle title={selectedType as string} alignLeft />
+      <PageTitle title={getTitle(selectedType as string)} alignLeft />
       <div className="grid grid-cols-2 gap-3 md:grid-cols-4">
         {isLoading && <ProductCardSkelton productsPerRow={4} />}
         {isError && <div>에러가 발생했습니다.</div>}
+        {data?.pages && data.pages[0].products.length === 0 && (
+          <div className="text-center col-span-full">
+            추가된 상품이 존재하지 않습니다.
+          </div>
+        )}
         {data?.pages &&
           data?.pages.flatMap((page) =>
             page.products.map((product) => (
